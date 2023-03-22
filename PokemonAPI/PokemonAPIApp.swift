be680d37d836f384a732c00ed6ddeb2f475517f2
David Cormell <d.cormell@etoncollege.org.uk>
2023-03-22 10:05:36 +0000

    Initial Commit

---
+++ PokemonAPI/PokemonAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAPIApp.swift
+//  PokemonAPI
+//
+//  Created by Cormell, David - DPC on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
